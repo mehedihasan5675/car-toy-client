d2a81107ce2f4c5064083eeb73bc42c4784e0fc4
mehedi hasan <mdchanchal5675@gmail.com>
2023-05-19 01:16:45 +0600

    job category design part update

--- src/components/pages/Home/shopByCategory/ShopCategory.jsx
+++ src/components/pages/Home/shopByCategory/ShopCategory.jsx
@@ -1,8 +1,10 @@
+import Lottie from "lottie-react";
 import React, { useEffect, useState } from "react";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import market from "../../../../assets/market.json";
+import truck from "../../../../assets/truck.json";
 import ShopCart from "./shopCart";
-
 const ShopCategory = () => {
     const [toys,setToys]=useState([])
     useEffect(()=>{
@@ -13,16 +15,25 @@ const ShopCategory = () => {
         })
     },[])
   return (
-    <div className="">
+    <div className="py-16">
       <h3
         style={{ borderBottomRightRadius: "40px", borderTopLeftRadius: "40px" }}
         className="font-bold text-2xl md:text-4xl italic tracking-wider text-center  px-5 py-2 bg-[#C4DFDF] text-gray-600 w-1/2 mx-auto  "
       >
         Shop by category
       </h3>
-
-      <div className="max-w-7xl mx-auto  px-5">
-        <Tabs>
+      
+      <div className="max-w-7xl relative mx-auto  px-5">
+      <div className="absolute -top-40 left-0 md:-top-44 md:left-32">
+      <Lottie 
+          animationData={truck} className="w-52 h-52" loop={true}></Lottie>
+      </div>
+      <div className="absolute -top-40 right-0">
+      <Lottie 
+          animationData={market} className="w-44 h-44" loop={true}></Lottie>
+      </div>
+        <Tabs >
+        
           <TabList style={{ borderBottomRightRadius: "40px", borderTopLeftRadius: "40px" }} className="bg-[#D2E9E9] mb-5 px-2 py-2 ">
             <Tab>Sports Cars</Tab>
             <Tab>Regular Cars</Tab>
