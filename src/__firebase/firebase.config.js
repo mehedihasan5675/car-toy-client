3a350c464b85bc791fb56bd1f22726e5554c304c
mehedi hasan <mdchanchal5675@gmail.com>
2023-05-19 16:15:00 +0600

    environment variable set up done

--- src/__firebase/firebase.config.js
+++ src/__firebase/firebase.config.js
@@ -5,13 +5,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA4RvH23WYnuz7hjNbmxxCWEh6BYoEtiU4",
-  authDomain: "car-toys-930ac.firebaseapp.com",
-  projectId: "car-toys-930ac",
-  storageBucket: "car-toys-930ac.appspot.com",
-  messagingSenderId: "188575392162",
-  appId: "1:188575392162:web:24174391b2063a187792fb"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
+console.log(import.meta.env.VITE_APPID);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
